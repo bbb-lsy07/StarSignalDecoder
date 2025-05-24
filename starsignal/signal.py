ea0544442d643a25aa6b48a2ee6c2845db9c32e1
bbb-lsy07 <pitas_hadron0w@icloud.com>
2025-05-24 17:45:06 +0800

    Update signal.py

--- starsignal/signal.py
+++ starsignal/signal.py
@@ -6,14 +6,13 @@ class SignalGenerator:
         self.difficulty = difficulty
         self.rules = {
             "easy": ["ignore_symbols"],
-            "medium": ["ignore_symbols", "reverse"],
-            "hard": ["ignore_symbols", "reverse", "repeat", "odd_plus_one"],
-            "challenge": ["ignore_symbols", "reverse", "repeat", "odd_plus_one", "even_only", "replace_char"]
+            "medium": ["ignore_symbols", "reverse", "odd_plus_one"],
+            "hard": ["ignore_symbols", "reverse", "odd_plus_one", "even_only", "replace_char"],
+            "challenge": ["ignore_symbols", "reverse", "odd_plus_one", "even_only", "replace_char"]
         }
         self.rule_texts = {
             "ignore_symbols": "忽略所有非数字字符",
             "reverse": "忽略非数字字符并反转序列",
-            "repeat": "忽略非数字字符并重复序列两次",
             "odd_plus_one": "忽略非数字字符，奇数数字加1",
             "even_only": "忽略非数字字符，仅保留偶数数字",
             "replace_char": "忽略非数字字符，替换5为0"
@@ -31,21 +30,28 @@ class SignalGenerator:
             else:
                 signal.append(digits[i])
         
-        rule = random.choice(self.rules[self.difficulty])
-        digit_sequence = [c for c in signal if c.isdigit()]
+        is_boss = length >= 12
+        if is_boss:
+            rule = random.sample(self.rules[self.difficulty], 2)
+            rule_text = "和".join(self.rule_texts[r] for r in rule)
+        else:
+            rule = [random.choice(self.rules[self.difficulty])]
+            rule_text = self.rule_texts[rule[0]]
         
-        if rule == "ignore_symbols":
-            answer = ''.join(digit_sequence)
-        elif rule == "reverse":
-            answer = ''.join(digit_sequence)[::-1]
-        elif rule == "repeat":
-            answer = ''.join(digit_sequence) * 2
-        elif rule == "odd_plus_one":
-            answer = ''.join(str((int(c) + 1) % 10) if int(c) % 2 == 1 else c for c in digit_sequence)
-        elif rule == "even_only":
-            answer = ''.join(c for c in digit_sequence if int(c) % 2 == 0)
-        else:  # replace_char
-            answer = ''.join('0' if c == '5' else c for c in digit_sequence)
+        digit_sequence = [c for c in signal if c.isdigit()]
+        answer = ''.join(digit_sequence)
+        
+        for r in rule:
+            if r == "ignore_symbols":
+                answer = ''.join(digit_sequence)
+            elif r == "reverse":
+                answer = answer[::-1]
+            elif r == "odd_plus_one":
+                answer = ''.join(str((int(c) + 1) % 10) if int(c) % 2 == 1 else c for c in answer)
+            elif r == "even_only":
+                answer = ''.join(c for c in answer if int(c) % 2 == 0)
+            elif r == "replace_char":
+                answer = ''.join('0' if c == '5' else c for c in answer)
         
         if not answer:
             answer = "0"
@@ -58,4 +64,4 @@ class SignalGenerator:
             distractors.append(distractor)
         
         strength = random.randint(1, 3)
-        return ''.join(signal), self.rule_texts[rule], answer, distractors, strength
+        return ''.join(signal), rule_text, answer, distractors, strength
