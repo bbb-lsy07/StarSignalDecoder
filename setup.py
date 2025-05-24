a83f23627bbbc3145ff31bf3308fd86c0a61a7f5
bbb-lsy07 <pitas_hadron0w@icloud.com>
2025-05-24 13:42:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="starsignal",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "starsignal = starsignal.cli:main",
+        ],
+    },
+    author="bbb-lsy07",
+    author_email="lisongyue0125@163.com",
+    description="星际迷航：信号解码 - 一个有趣的终端解谜游戏",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bbb-lsy07/StarSignalDecoder",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
